3f51a3511c8f580de242c8bde3256c562557a7ed
CarlosPif <camartpif@gmail.com>
2025-07-03 13:36:59 +0200

    hola

--- my_app/Home.py
+++ my_app/Home.py
@@ -3,7 +3,7 @@ import streamlit as st
 #poner aqui titulo y fotillo decelera
 
 if st.button("See application stats"):
-    st.switch_page("Applications.py")
+    st.switch_page("pages/Applications.py")
 
 if st.button("See scoring stats"):
-    st.switch_page("Results.py")
+    st.switch_page("pages/Results.py")
