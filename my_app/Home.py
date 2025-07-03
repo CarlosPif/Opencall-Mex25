d6428ca4c9377e49f8254a6dcaa29b1b16aaa5b7
CarlosPif <camartpif@gmail.com>
2025-07-03 13:49:50 +0200

    hola

--- my_app/Home.py
+++ my_app/Home.py
@@ -2,8 +2,35 @@ import streamlit as st
 
 #poner aqui titulo y fotillo decelera
 
-if st.button("See application stats"):
-    st.switch_page("pages/Applications.py")
+st.set_page_config(
+    page_title="Startup Program Feedback Dashboard",
+    layout="wide"
+)
 
-if st.button("See scoring stats"):
-    st.switch_page("pages/Results.py")
+st.markdown("""
+    <style>
+    div.stButton > button {
+        background-color: #FF4B4B;
+        color: white;
+        border-radius: 10px;
+        padding: 0.5em 1em;
+        font-size: 16px;
+    }
+    div.stButton > button:hover {
+        background-color: #D43F3F;
+        color: white;
+    }
+    </style>
+    """, unsafe_allow_html=True)
+
+st.image("https://images.squarespace-cdn.com/content/v1/67811e8fe702fd5553c65249/749161ba-4abb-4514-9642-edc82c1c9c9d/Decelera-Logo.png?format=1500w", width=500)
+
+cols = st.columns(2)
+
+with cols[0]:
+    if st.button("See application stats"):
+        st.switch_page("pages/Applications.py")
+
+with cols[1]:
+    if st.button("See scoring stats"):
+        st.switch_page("pages/Results.py")
