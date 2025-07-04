60c88041edf6488b251e402dd8b7ccd3e634c5e2
CarlosPif <camartpif@gmail.com>
2025-07-04 09:11:57 +0200

    siu

--- my_app/pages/Results.py
+++ my_app/pages/Results.py
@@ -1,3 +1,35 @@
+from pyairtable import Api
+import pandas as pd
 import streamlit as st
+import plotly.express as px
+import numpy as np
+import datetime
 
-st.title("holaa")
+# Configuracion de AirTable
+
+api_key = st.secrets["airtable"]["api_key"]
+base_id = st.secrets["airtable"]["base_id"]
+table_id = st.secrets["airtable"]["table_id"]
+
+api = Api(api_key)
+table = api.table(base_id, table_id)
+
+# Obtenemos los datos
+records = table.all(view='Applicants Mex25')
+data = [record['fields'] for record in records]
+df = pd.DataFrame(data)
+
+def fix_cell(val):
+    if isinstance(val, dict) and "specialValue" in val:
+        return float("nan")
+    return val
+
+df = df.applymap(fix_cell)
+
+# Comenzamos con el dashboard
+st.set_page_config(
+    page_title="Opencall Dashboard Decelera Mexico 2025",
+    layout="wide"
+)
+
+st.markdown("**<h1 style='text-align: center;'>Open Call Decelera Mexico 2025</h1>**", unsafe_allow_html=True)
