f3f2074e3998fead8a2f11d2d19051767c30ccee
Carlos Martinez <cmartinez@sancuscapital.es>
2025-07-30 15:19:33 +0200

    helo

--- my_app/pages/Results.py
+++ my_app/pages/Results.py
@@ -282,16 +282,19 @@ ph3_passed       = ph3_df[ph3_df['PH3_Final_Score'] >= ph3_q3].shape[0]
 #vamos con los de la fase 4
 df_4 =df.dropna(subset=['Judges_Average'])
 
-ph4_in_progress = df[df['Status'] == 'PH4_Pending_Judge_Assignment'].shape[0]
+ph4_in_progress = df[
+    (df['Status'] == 'PH4_Pending_Judge_Assignment') |
+    (df['Status'] == 'PH4_Judge_Evaluation')
+    ].shape[0]
 df_4['Judges_Average'] = pd.to_numeric(df_4['Judges_Average'], errors='coerce')
 
 ph4_q1 = np.percentile(df_4['Judges_Average'], 25)
 ph4_q2 = np.percentile(df_4['Judges_Average'], 50)
 ph4_q3 = np.percentile(df_4['Judges_Average'], 75)
 
-ph4_rejection = df_4[df_4['Judges_Average'] < ph4_q2].shape[0]
+ph4_rejection = df_4[(df_4['Judges_Average'] < ph4_q2) & (df_4['Judges_Completed_Count'] >= 2)].shape[0]
 ph4_waiting_list = df_4[(df_4['Judges_Average'] > ph4_q1) & (df_4['Judges_Average'] < ph4_q3)].shape[0]
-ph4_passed = df_4[(df_4['Judges_Average'] >= ph4_q2)].shape[0]
+ph4_passed = df_4[(df_4['Judges_Average'] >= ph4_q2) & (df_4['Judges_Completed_Count'] >= 2)].shape[0]
 
 with cols[1]:
     st.markdown("**<p style='text-align: center;'>Funnel situation based on percentiles (not real, just for research)</p>**", unsafe_allow_html=True)
@@ -681,6 +684,125 @@ for _, row in top_10.iterrows():
 <td style="padding: 8px;">{deck_link}</td>
 <td style="padding: 8px;">{row['Judge Evaluation Average']}</td>
 <td style="padding: 8px;">{judges}</td>
+</tr>
+"""
+
+html_table += """
+</tbody>
+</table>
+</div>
+"""
+
+st.markdown(html_table, unsafe_allow_html=True)
+
+#=======================A ver un scatter plot para analizar las diferencias de puntuaciones==================
+
+df['distancia'] = abs(df['PH3_Final_Score'] - df['Judges_Average']) / np.sqrt(2)
+top_discrep = df.sort_values(by='distancia', ascending=False).head(10)
+
+fig = go.Figure()
+
+fig.add_traces(go.Scatter(
+    x=df['PH3_Final_Score'],
+    y=df['Judges_Average'],
+    text=df['Startup name'],
+    mode='markers',
+    marker=dict(
+        color=df['distancia'],
+        colorscale='Viridis',
+        colorbar=dict(title='distancia')
+    )
+))
+
+fig.add_traces(go.Scatter(
+    x=top_discrep['PH3_Final_Score'],
+    y=top_discrep['Judges_Average'],
+    text=top_discrep['Startup name'],
+    mode='text',
+    textposition='top right',
+    name='Top 10 Discrepantes',
+))
+
+fig.add_traces(go.Scatter(
+    x=[0,5],
+    y=[0,5],
+    mode='lines'
+))
+
+fig.update_layout(
+    xaxis=dict(
+        title='Team Evaluation',
+        range=[2.5, 5]
+    ),
+    yaxis=dict(
+        title='Judges Evaluation',
+        range=[1, 5]
+    ),
+    showlegend=False
+)
+
+st.plotly_chart(fig)
+
+top_distance = df.sort_values(by='distancia', ascending=False).head(10)
+top_distance['distancia'] = top_distance['distancia'].apply(lambda x: round(x, 2))
+
+top_distance = top_distance.rename(columns={
+    'deck_URL': 'Deck (url)',
+    'distancia': 'Distance to having the same score',
+    'Judges_Average': 'Judge Evaluation Average'
+})
+
+html_table = """
+<div style="overflow-x: auto; width: 100%;">
+<table style="width: 100%; border-collapse: collapse;">
+<thead>
+<tr>
+<th style="text-align: center; padding: 8px;">Startup</th>
+<th style="text-align: center; padding: 8px;">Deck (doc)</th>
+<th style="text-align: center; padding: 8px;">Deck (link)</th>
+<th style="text-align: center; padding: 8px;">Judge Evaluation</th>
+<th style="text-align: center; padding: 8px;">Judges</th>
+<th style="text-align: center; padding: 8px;">Team Evaluation</th>
+<th style="text-align: center; padding: 8px;">Evaluator</th>
+<th style="text-align: center; padding: 8px;">Distance</th>
+</tr>
+</thead>
+<tbody>
+"""
+
+for _, row in top_distance.iterrows():
+    website_link = (
+        f"<a href='{row['Deck (doc)']}' target='_blank'>"
+        f"<img src='{row['deck_icon']}' alt='Website' width='40' style='vertical-align: middle;'/>"
+        "</a>"
+        if pd.notna(row['Deck (doc)']) and pd.notna(row['deck_icon']) else ""
+    )
+    deck_link = (
+        f"<a href='{row['Deck (url)']}' target='_blank'>{row['Deck (url)']}</a>"
+        if pd.notna(row['Deck (url)']) else ""
+    )
+    judges = (
+        f"<p>{row['Judges_Evaluated']}</p>"
+        if pd.notna(row['Judges_Evaluated']) else ""
+    )
+    evaluators = (
+        f"<p>{row['PH3_Evaluator_1st']}, {row['PH3_Evaluator_2nd']}</p>"
+        if pd.notna(row['PH3_Evaluator_1st']) and pd.notna(row['PH3_Evaluator_2nd']) else ""
+    )
+    distance = (
+        f"<p>{row['Distance to having the same score']}</p>"
+        if pd.notna(row['Distance to having the same score']) else ""
+    )
+    html_table += f"""
+<tr>
+<td style="padding: 8px;">{row['Startup name']}</td>
+<td style="padding: 8px;">{website_link}</td>
+<td style="padding: 8px;">{deck_link}</td>
+<td style="padding: 8px;">{row['Judge Evaluation Average']}</td>
+<td style="padding: 8px;">{judges}</td>
+<td style="padding: 8px;">{round(row['PH3_Final_Score'], 2)}</td>
+<td style="padding: 8px;">{evaluators}</td>
+<td style="padding: 8px;">{distance}</td>
 </tr>
 """
 
