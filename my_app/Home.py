9bb8ee87f37cab51f562cd17b1203f424d3d54d9
Carlos Martinez <cmartinez@sancuscapital.es>
2025-09-24 13:51:01 +0200

    v

--- my_app/Home.py
+++ my_app/Home.py
@@ -34,12 +34,3 @@ st.markdown("""
 st.markdown("**<h1 style='text-align: center;'>Open Call</h1>**", unsafe_allow_html=True)
 st.markdown("**<h1 style='text-align: center;'>Decelera Mexico 2025</h1>**", unsafe_allow_html=True)
 #st.image("https://images.squarespace-cdn.com/content/v1/67811e8fe702fd5553c65249/749161ba-4abb-4514-9642-edc82c1c9c9d/Decelera-Logo.png?format=1500w", width=500)
-
-cols = st.columns([1, 2, 1])
-
-with cols[1]:
-    if st.button("Application Metrics"):
-        st.switch_page("pages/Applications.py")
-
-    if st.button("Score Metrics"):
-        st.switch_page("pages/Results.py")
